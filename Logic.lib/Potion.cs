160018e95c270b98c04606de8300257c86107d7b
Ethan <engerethan@gmail.com>
2022-03-24 21:30:29 -0600

    Item Usage Tests Green

--- Logic.lib/Potion.cs
+++ Logic.lib/Potion.cs
@@ -6,9 +6,20 @@ public class Potion : BaseItem
     {
         switch(strength)
         {
-            case Strength.Used:
+            case Strength.Max:
+            pokemon.Heal(pokemon.Max_HP);
             break;
-
+            case Strength.Hyper:
+            pokemon.Heal(200);
+            break;
+            case Strength.Super:
+            pokemon.Heal(50);
+            break;
+            case Strength.Basic:
+            pokemon.Heal(20);
+            break;
+            default:
+            break;
         }
         base.UseItem(pokemon);
     }
